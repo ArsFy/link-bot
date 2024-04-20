5036db8861f84388a1fdd763420cf57ef6539730
Arsfy <arsfy@outlook.com>
2024-04-21 01:28:36 +0800

    debug

--- pixiv.js
+++ pixiv.js
@@ -50,36 +50,48 @@ module.exports = {
                     const tags = [];
                     for (let i in illust.tags.tags) tags.push(illust.tags.tags[i].tag.replace("-", '').replace(":", '').replace(/\(.*?\)/g, ''))
 
-                    axios({
-                        method: 'GET',
-                        url: `https://www.pixiv.net/ajax/illust/${illust.id}/pages`,
-                        headers: {
-                            ...headers,
-                            'referer': 'https://www.pixiv.net/'
-                        },
-                    }).then(async res => {
-                        const filenames = []
-                        for (const i in res.data.body) {
-                            const filename = path.join(__dirname, "image", `${illust.id}-${i}.${res.data.body[i].urls.original.split(".").pop()}`)
-                            filenames.push(filename)
-                            await downloads(filename, res.data.body[i].urls.original)
-                        }
+                    MongoPool.getInstance().then(async client => {
+                        const collection = client.db(config.DB_NAME).collection("pixiv-images");
+                        const res = await collection.find({ id: String(illust.id) }).toArray();
 
-                        MongoPool.getInstance().then(async client => {
-                            const collection = client.db(config.DB_NAME).collection("pixiv-images");
-                            await collection.insertOne({
-                                id: illust.id,
-                                title: illust.title,
-                                userId: illust.userId,
-                                userName: illust.userName,
-                                tags,
-                                filenames
+                        if (res.length > 0) {
+                            const filenames = res[0].filenames.map(filename => path.join(__dirname, "image", filename));
+                            callback(`ID: [${res[0].id}](https://pixiv.net/i/${res[0].id})\nTitle: ${res[0].title}\nUser: [${res[0].userName}](https://pixiv.net/users/${res[0].userId})\n\nTags: #${res[0].tags.join('  #')}`, filenames, chatId)
+                        } else {
+                            axios({
+                                method: 'GET',
+                                url: `https://www.pixiv.net/ajax/illust/${illust.id}/pages`,
+                                headers: {
+                                    ...headers,
+                                    'referer': 'https://www.pixiv.net/'
+                                },
+                            }).then(async res => {
+                                const filenames = []
+                                for (const i in res.data.body) {
+                                    const filename = path.join(__dirname, "image", `${illust.id}-${i}.${res.data.body[i].urls.original.split(".").pop()}`)
+                                    filenames.push(filename)
+                                    await downloads(filename, res.data.body[i].urls.original)
+                                }
+
+                                MongoPool.getInstance().then(async client => {
+                                    const collection = client.db(config.DB_NAME).collection("pixiv-images");
+                                    await collection.insertOne({
+                                        id: illust.id,
+                                        title: illust.title,
+                                        userId: illust.userId,
+                                        userName: illust.userName,
+                                        tags,
+                                        filenames
+                                    })
+                                }).catch(err => {
+                                    console.error(err)
+                                })
+
+                                callback(`ID: [${illust.id}](https://pixiv.net/i/${illust.id})\nTitle: ${illust.title}\nUser: [${illust.userName}](https://pixiv.net/users/${illust.userId})\n\nTags: #${tags.join('  #')}`, filenames, chatId)
+                            }).catch(err => {
+                                console.error(err)
                             })
-                        }).catch(err => {
-                            console.error(err)
-                        })
-
-                        callback(`ID: [${illust.id}](https://pixiv.net/i/${illust.id})\nTitle: ${illust.title}\nUser: [${illust.userName}](https://pixiv.net/users/${illust.userId})\n\nTags: #${tags.join('  #')}`, filenames, chatId)
+                        }
                     }).catch(err => {
                         console.error(err)
                     })
@@ -93,10 +105,10 @@ module.exports = {
                 if (isNaN(urlId)) { get() } else {
                     MongoPool.getInstance().then(async client => {
                         const collection = client.db(config.DB_NAME).collection("pixiv-images");
-                        const res = await collection.find({ id: urlId }).toArray();
+                        const res = await collection.find({ id: String(urlId) }).toArray();
 
                         if (res.length > 0) {
-                            const filenames = res[0].filenames.map(filename => path.join(__dirname, "image", filename));
+                            const filenames = res[0].filenames.map(filename => filename);
                             callback(`ID: [${res[0].id}](https://pixiv.net/i/${res[0].id})\nTitle: ${res[0].title}\nUser: [${res[0].userName}](https://pixiv.net/users/${res[0].userId})\n\nTags: #${res[0].tags.join('  #')}`, filenames, chatId)
                         } else get()
                     }).catch(err => {
